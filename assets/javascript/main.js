a7d40b5e16f859873f064c620ac56d8688e4a295
Wertzhog <ljupko.jurcevic@gmail.com>
2020-08-16 17:11:54 +0100

    added js functions for checking for match

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,14 +1,71 @@
 const cards = document.querySelectorAll(".card");
 
-
-
 cards.forEach(card => card.addEventListener('click', cardSelected));
 
 
 
+let hasSelectedCard = false;
+let firstCard, secondCard;
+let lockGameArea = false;//disables clicking more then two cards
+
+
+
 function cardSelected (){
-   this.classList.toggle("selected");
-   var x = this.children[2];
-   x.play();
+   if(lockGameArea === true) return;
+
+   this.classList.add("selected");
+   //let sound = this.children[2];
+   //sound.play();
+
+   if(!hasSelectedCard){
+
+      hasSelectedCard = true;
+      firstCard = this;
+   }
+   else{
+      
+      hasSelectedCard = false;
+      secondCard = this;
+
+
+      checkForMatch();
+   }
+   
 }
 
+function checkForMatch(){
+
+   if(firstCard.dataset.sound === secondCard.dataset.sound){
+      matchedSounds();
+   }
+   else{
+      
+      mismatchedSounds();
+   }
+}
+
+function matchedSounds(){
+      firstCard.removeEventListener('click',cardSelected);
+      secondCard.removeEventListener('click',cardSelected);
+
+      firstCard.classList.remove("selected");
+      secondCard.classList.remove("selected");
+
+      firstCard.classList.add("matched");
+      secondCard.classList.add("matched");
+      //add sounds
+}
+
+function mismatchedSounds(){
+
+   lockGameArea = true;
+
+   setTimeout(() => {
+      firstCard.classList.remove("selected");
+      secondCard.classList.remove("selected");
+
+      lockGameArea = false;
+      },1000);
+
+      
+}
