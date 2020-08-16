d0f3b3d3d7ce348d9abe5252fca8de9e3456a141
Wertzhog <ljupko.jurcevic@gmail.com>
2020-08-16 20:40:44 +0100

    added audio class object,shuffle&reset functions

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,7 +2,8 @@ const cards = document.querySelectorAll(".card");
 
 cards.forEach(card => card.addEventListener('click', cardSelected));
 
-
+//reset button
+document.getElementById("restart").addEventListener('click', resetGame);
 
 let hasSelectedCard = false;
 let firstCard, secondCard;
@@ -13,9 +14,13 @@ let lockGameArea = false;//disables clicking more then two cards
 function cardSelected (){
    if(lockGameArea === true) return;
 
+   if(this === firstCard) return; // to prevent double clicking same card
+
    this.classList.add("selected");
+
+   audio.clickedCardAudio.play();
    //let sound = this.children[2];
-   //sound.play();
+  // sound.play();
 
    if(!hasSelectedCard){
 
@@ -53,6 +58,8 @@ function matchedSounds(){
 
       firstCard.classList.add("matched");
       secondCard.classList.add("matched");
+
+      resetBoard();
       //add sounds
 }
 
@@ -64,8 +71,37 @@ function mismatchedSounds(){
       firstCard.classList.remove("selected");
       secondCard.classList.remove("selected");
 
-      lockGameArea = false;
+      resetBoard();
       },1000);
 
       
 }
+
+function resetBoard(){
+   [hasSelectedCard, lockGameArea] = [false, false];
+   [firstCard,secondCard] = [null, null];
+}
+//reset button
+function resetGame(){
+   
+}
+
+(function shuffle(){
+   cards.forEach(card => {
+      let randOrder = Math.floor(Math.random() * 12);
+      card.style.order = randOrder;
+   });
+
+})();
+
+//audio controll
+
+let audio = {
+   introMusic : new Audio("assets/audio/startmusic.mp3"),
+   loseAudio : new Audio("assets/audio/losesound.mp3"),
+   winAudio : new Audio("assets/audio/winsound.mp3"),
+   matchAudio : new Audio("assets/audio/matchsound.mp3"),
+   lvlUpAudio : new Audio("assets/audio/lvlupsound.mp3"),
+   clickedCardAudio : document.querySelectorAll('.card').children[2],
+
+};
