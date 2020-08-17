d7fa6a89c64dc066eeed442b0759f8e50c4319c1
Wertzhog <ljupko.jurcevic@gmail.com>
2020-08-17 18:20:22 +0100

    added audio match function and testing code for bugs

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,53 @@
+//Check if DOMContent is loaded
+
+if (document.readyState === 'loading') {
+   document.addEventListener('DOMContentLoaded', ready());
+ } else {
+   ready();
+ }
+
+
+//audio controll
+
+class AudiControll  {
+   constructor() {
+   this.introMusic = new Audio("assets/audio/startmusic.mp3");
+   this.introMusic.volume = 0.1;
+   this.loseAudio = new Audio("assets/audio/losesound.mp3");
+   this.winAudio = new Audio("assets/audio/winsound.mp3");
+   this.matchAudio = new Audio("assets/audio/matchsound.mp3");
+   this.lvlUpAudio = new Audio("assets/audio/lvlupsound.mp3");
+   this.clickedCardAudio = document.querySelectorAll('card');
+   }
+
+   loseSound(){
+      this.loseAudio.play();
+   }
+
+   winMusic(){
+      this.winAudio.play();
+   }
+   matchSound(){
+      this.matchAudio.play();
+   }
+
+   lvlUpSound(){
+      this.lvlUpAudio.play();
+   }
+
+}
+
+///Ready game function
+function ready(){
+   document.getElementsByClassName("overlay-start")[0].classList.add("visible");
+   
+}
+
 const cards = document.querySelectorAll(".card");
 
 cards.forEach(card => card.addEventListener('click', cardSelected));
 
-//reset button
-document.getElementById("restart").addEventListener('click', resetGame);
+document.getElementsByClassName("overlay-start")[0].addEventListener('click', startGame);
 
 let hasSelectedCard = false;
 let firstCard, secondCard;
@@ -17,20 +61,23 @@ function cardSelected (){
    if(this === firstCard) return; // to prevent double clicking same card
 
    this.classList.add("selected");
+   
+   
 
-   audio.clickedCardAudio.play();
-   //let sound = this.children[2];
-  // sound.play();
+   let sound = this.children[2];
+   sound.play();
 
    if(!hasSelectedCard){
 
       hasSelectedCard = true;
       firstCard = this;
+      
    }
    else{
       
       hasSelectedCard = false;
       secondCard = this;
+      
 
 
       checkForMatch();
@@ -61,6 +108,15 @@ function matchedSounds(){
 
       resetBoard();
       //add sounds
+      setTimeout(() => {
+         matchSound();
+      }, 500);
+      
+}
+
+function matchSound(){
+   let audioC = new AudiControll();
+      audioC.matchSound();
 }
 
 function mismatchedSounds(){
@@ -81,10 +137,16 @@ function resetBoard(){
    [hasSelectedCard, lockGameArea] = [false, false];
    [firstCard,secondCard] = [null, null];
 }
-//reset button
-function resetGame(){
+//reset button function ???
+function startGame(){
+
+  //let elem = document.getElementsByClassName("overlay-start")[0];
+  this.classList.remove("visible");
+  
+   
    
 }
+
 
 (function shuffle(){
    cards.forEach(card => {
@@ -94,14 +156,3 @@ function resetGame(){
 
 })();
 
-//audio controll
-
-let audio = {
-   introMusic : new Audio("assets/audio/startmusic.mp3"),
-   loseAudio : new Audio("assets/audio/losesound.mp3"),
-   winAudio : new Audio("assets/audio/winsound.mp3"),
-   matchAudio : new Audio("assets/audio/matchsound.mp3"),
-   lvlUpAudio : new Audio("assets/audio/lvlupsound.mp3"),
-   clickedCardAudio : document.querySelectorAll('.card').children[2],
-
-};
